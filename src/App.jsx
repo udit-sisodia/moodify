a7a170277f2c0f2435f98594b0afb75f26530239
udit-sisodia <uditsisodia1234@gmail.com>
2026-04-17 17:47:51 +0530

    starting fresh

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// import './App.css'
+import FaceExpression from './features/Expression/components/FaceExpression'
+
+function App() {
+
+  return (
+   <FaceExpression/>
+  )
+}
+
+export default App
